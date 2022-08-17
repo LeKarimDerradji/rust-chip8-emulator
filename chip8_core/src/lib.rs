ba4f4693b49fe94801c190071277d0e6e8e91faf
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-08-17 16:23:21 +0200

    pub_const_v_regs

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -1,10 +1,13 @@
 
-const SCREEN_WIDTH: usize = 64;
-const SCREEN_HEIGHT: usize = 32;
+pub const SCREEN_WIDTH: usize = 64;
+pub const SCREEN_HEIGHT: usize = 32;
+
 const RAM_SIZE: usize = 4096;
+const NUM_REGS: usize = 16;
 
 pub struct Emu {
     pc: u16,
     ram: [u8; RAM_SIZE],
     screen: [bool; SCREEN_WIDTH * SCREEN_HEIGHT],
+    v_reg: [u8; NUM_REGS],
 }
